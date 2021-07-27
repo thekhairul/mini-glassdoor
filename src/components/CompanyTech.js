e0258e71cc0271acae415c8d0c49a3c80b8ce18d
thekhairul <thekhairul.office@gmail.com>
2021-07-27 23:47:51 +0600

    custom tech adding feature

--- src/components/CompanyTech.js
+++ src/components/CompanyTech.js
@@ -1,11 +1,35 @@
-import { Col, Row, Select } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import { Col, Divider, Input, Row, Select } from 'antd';
 import { useState } from "react";
 
 const techOptions = ['NodeJs','Laravel','React','Vue','Typescript','MySQL','PostgreSQL','MongoDB'];
 
 export default function CompanyTech() {
+    const [techList, setTechList] = useState(techOptions);
     const [selectedTech, setTechSelection] = useState([]);
-    const unselectedTechs = techOptions.filter(tech => !selectedTech.includes(tech));
+    const [customOption, setCustomOption] = useState('');
+    const unselectedTechs = techList.filter(tech => !selectedTech.includes(tech));
+    const addCustomOption = (e) => {
+        if (e.code !== 'Enter' || !customOption) return;
+        setTechList(currentTechs => ([...currentTechs, customOption]));
+        setCustomOption('');
+    }
+
+    const dropdownRender = menu => (
+          <div>
+            {menu}
+            <Divider style={{ margin: '4px 0' }} />
+            <div style={{ display: 'flex', flexWrap: 'nowrap', padding: 8 }}>
+              <Input style={{ flex: 'auto' }} value={customOption} onChange={(e) => setCustomOption(e.target.value)} onKeyDown={addCustomOption}/>
+              <a
+                style={{ flex: 'none', padding: '8px', display: 'block', cursor: 'pointer' }}
+                onClick={addCustomOption}
+              >
+                <PlusOutlined /> Add item
+              </a>
+            </div>
+          </div>
+        );
 
     return (
         <Row justify="center">
@@ -17,10 +41,11 @@ export default function CompanyTech() {
                     placeholder="Inserted are removed"
                     value={selectedTech}
                     onChange={setTechSelection}
+                    dropdownRender={dropdownRender}
                     style={{ width: '100%' }}
                 >
-                    {unselectedTechs.map(item => (
-                        <Select.Option key={item} value={item}>
+                    {unselectedTechs.map((item, idx) => (
+                        <Select.Option key={idx} value={item}>
                             {item}
                         </Select.Option>
                     ))}

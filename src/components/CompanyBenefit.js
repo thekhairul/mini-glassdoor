b4412446f300eabdfd39d9e76af881e02155d5b3
thekhairul <thekhairul.office@gmail.com>
2021-07-30 17:00:00 +0600

    apply custom option hook to benefit and industry section

--- src/components/CompanyBenefit.js
+++ src/components/CompanyBenefit.js
@@ -1,10 +1,13 @@
 import { Col, Row, Select } from 'antd';
 import { useState } from "react";
+import { useAntDropdownRender } from './hooks';
 
 const benefitOptions = ['5 days work week', '2 festival bonus', 'Yearly salary review', 'Profit share', 'Gratuity', 'Subsidized lunch & snacks', '21 days yearly paid leave'];
 
 export default function CompanyBenefit() {
+    const [benefitList, setBenifitList] = useState(benefitOptions);
     const [selectedBenefits, setBenefitsSelection] = useState([]);
+    const dropdownRender = useAntDropdownRender(setBenifitList);
 
     return (
         <Row justify="center">
@@ -16,10 +19,11 @@ export default function CompanyBenefit() {
                     placeholder="Pick or add benefits"
                     value={selectedBenefits}
                     onChange={setBenefitsSelection}
+                    dropdownRender={dropdownRender}
                     style={{ width: '100%' }}
                 >
-                    {benefitOptions.map(item => (
-                        <Select.Option key={item} value={item}>
+                    {benefitList.map((item, idx) => (
+                        <Select.Option key={idx} value={item}>
                             {item}
                         </Select.Option>
                     ))}
--- src/components/CompanyDomain.js
+++ src/components/CompanyDomain.js
@@ -1,10 +1,13 @@
 import { Col, Row, Select } from 'antd';
 import { useState } from "react";
+import { useAntDropdownRender } from './hooks';
 
 const industryOptions = ['Banking and Finance','Health and Medicine','Media & Marketing','Education'];
 
 export default function CompanyDomain() {
+    const [industryList, setIndustryList] = useState(industryOptions);
     const [selectedIndustries, setIndustriesSelection] = useState([]);
+    const dropdownRender = useAntDropdownRender(setIndustryList);
 
     return (
         <Row justify="center">
@@ -16,10 +19,11 @@ export default function CompanyDomain() {
                     placeholder="Pick one or multiple domain"
                     value={selectedIndustries}
                     onChange={setIndustriesSelection}
+                    dropdownRender={dropdownRender}
                     style={{ width: '100%' }}
                 >
-                    {industryOptions.map(item => (
-                        <Select.Option key={item} value={item}>
+                    {industryList.map((item,idx) => (
+                        <Select.Option key={idx} value={item}>
                             {item}
                         </Select.Option>
                     ))}

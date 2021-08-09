02c9968d4633343724f491965ce0b700960ee5a2
thekhairul <thekhairul.office@gmail.com>
2021-08-09 15:50:18 +0600

    init redux for state management

--- todo.config.js
+++ todo.config.js
@@ -1,2 +1,4 @@
 // TODO: move addCompany to an ant drawer ✅
-// TODO: add rich text editor for review section
+// TODO: add rich text editor for review section ✅
+// TODO: setup state management
+// TODO: add form validation
